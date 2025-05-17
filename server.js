0769aa7fbd386e00122789857873b1f7f9057a84
ShahlaaBee <shahlaa.ah@gmail.com>
2025-05-17 11:08:22 +0300

    updates to user

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger         = require('morgan');
 const methodOverride = require('method-override');
 const session        = require('express-session');
 const db             = require('./db');
+const userRouter = require('./routes/userRouter.js')
 
 require('dotenv').config();
 
@@ -21,6 +22,8 @@ app.use(session({
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+app.use('/users', userRouter)
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
