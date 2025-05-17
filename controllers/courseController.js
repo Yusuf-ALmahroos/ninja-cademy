b152527d703061975878368c01751cf9e02b7329
ShahlaaBee <shahlaa.ah@gmail.com>
2025-05-17 13:32:29 +0300

    user functions updated

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -14,6 +14,44 @@ const addCourse = async (req, res) => {
   }
 }
 
+const getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({})
+    res.send(courses)
+  } catch (error) {
+    console.error('An error has occurred retrieving your courses!', error.message)
+  }
+}
+
+const getCourseById = async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.id)
+    res.send(course)
+  } catch (error) {
+    console.error('An error has occurred retrieving the specified course!', error.message)
+  }
+}
+
+const updateCourseStatus = async (req, res) => {
+  try {
+    const course = await Course.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.send(course)
+  } catch (error) {
+    console.error('An error has occurred updating a course!', error.message)
+  }
+}
+
+const deleteCourse = async (req, res) => {
+  try {
+    await Course.findByIdAndDelete(req.params.id)
+  } catch (error) {
+    res.send(`Course has been deleted successfully!`)
+  }
+}
 module.exports = {
-  addCourse
+  addCourse,
+  getAllCourses,
+  getCourseById,
+  updateCourseStatus,
+  deleteCourse
 }
