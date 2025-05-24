91afaacc657d7ddf7e4368360ae82c74bf2a2094
Yousif Shukri <yousif4shukri@gmail.com>
2025-05-24 15:31:58 +0300

    removed users enrolled

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,6 +1,6 @@
 const User = require('../models/user.js')
 const Course = require('../models/course.js')
-
+const Lesson = require('../models/lesson.js')
 
 const addCourse = async (req, res) => {
   try {
@@ -12,13 +12,14 @@ const addCourse = async (req, res) => {
   }
 }
 
-const getAllCourses = async (req, res) => {
+const getAllCourses = async (req, res) => {   
   try {
-    const courses = await Course.find({})
-    res.send(courses)
+    const allCourses = await Course.find()
+    .populate('lessons')  // <-- added this to show lesson
+    res.render('courses/all.ejs', { courses: allCourses });
   } catch (error) {
-    console.error('An error has occurred retrieving your courses!', error.message)
-  }
+    console.error("error in getting all courses", error.message)
+  }          
 }
 
 const getCourseById = async (req, res) => {
@@ -50,9 +51,12 @@ const deleteCourse = async (req, res) => {
 const getCourseByTitle = async (req, res) => {
   try {
     const course = await Course.findOne({title: req.params.title})
-    res.render('./courses/course.ejs', {course});
+    const stringIds = course.lessons.map(id => id.toString())
+    const lessons = await Lesson.find({ _id: { $in: stringIds} })
+    const lessonsTitles = lessons.map(lesson => lesson.title);
+    res.render('./courses/course.ejs', {course, lessonsTitles});
   } catch (error) {
-    console.error("error finding course by title");
+    console.error("error finding course by title", error.message);
   }
 }
 
--- models/course.js
+++ models/course.js
@@ -4,7 +4,6 @@ const courseSchema = new mongoose.Schema({
   categories:    [{type: String, required: true}],
   periodInWeeks: {type: Number, required: true},
   title:         {type: String, required: true},
-  users:         [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
   lessons:       [{type: mongoose.Schema.Types.ObjectId, ref: 'Lesson'}]
 }, {timestamps: true})
 
